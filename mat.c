a96a779593bce010d1a743cb7ef1f020ce20e530
Madhesh S <madheshit2027@gmail.com>
2025-07-07 15:17:00 +0000

    mat.c

--- mat.c
+++ mat.c
@@ -11,20 +11,12 @@ int main(){
    }
    for(i=0;i<m;i++){
       for(j=0;j<n;j++){
-         array[i][j] = arr[i][j];
-         printf("%d\t",arr[i][j]);}
-      printf("\n");
-   }
-   for(i=0;i<m;i++){
-      for(j=0;j<n;j++){
          if(arr[i][j] == 0){
             for(k=0;k<m;k++){
                array[i][k] = 0;
-               printf("i = %d k = %d\n",i,k);
             }
             for(k=0;k<n;k++){
                array[k][j] = 0;
-               printf("k = %d j = %d\n",k,j);
             }
          }
       }
